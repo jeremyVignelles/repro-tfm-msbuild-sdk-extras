644fa618a08bfaee975bf71afff6e27c19628675
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2021-12-09 14:50:45 +0100

    Pushed repro

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestTFM {
+    public static class Program {
+        public static void Main() {
+            #if NET6_0
+            Console.WriteLine("NET6_0");
+            #endif
+
+            #if NET6_0_OR_GREATER
+            Console.WriteLine("NET6_0_OR_GREATER");
+            #endif
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # repro-tfm-msbuild-sdk-extras
+
+To repro:
+
+- Clone this project
+- `dotnet build`
+- `./bin/Debug/net6.0/TestTFM.exe`: See that only NET_6_0 is shown
+- `./bin/Debug/net6.0-windows/TestTFM.exe`: See that no message is shown
+
+Expected:
+```
+NET6_0
+NET6_0_OR_GREATER
+```
+
+for both TFM
